ed00661f53216ffe26c832af44147e445eafd7dd
Archana Nayak <archananayak212002@gmail.com>
2025-06-19 08:24:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function fibonacci(num) {
-// your code here
+    if (num === 1) return 0;
+    if (num === 2) return 1;
+
+    let a = 0, b = 1, result;
+    for (let i = 3; i <= num; i++) {
+        result = a + b;
+        a = b;
+        b = result;
+    }
+    return result;
 }
 
 module.exports = fibonacci;
+
